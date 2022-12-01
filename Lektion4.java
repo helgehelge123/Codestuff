7ef83633ec0a2be8fa81a71ab714733e1751cabb
helgehelge123 <helge-n@gmx.net>
2022-12-01 13:16:19 +0100

    File generated

---
+++ Lektion4.java
@@ -0,0 +1,50 @@
+public class Lektion4 {
+
+/** Inch to cm for fun
+//Inch to cm for fun
+
+    public static double berechneCm() {
+      int a =6;
+      double inch =2.56;
+    return inch*a;
+    }
+    
+    public static void main(String[] args) {
+      System.out.println(berechneCm());
+  }
+*/
+
+public static void main(String[] args) {
+
+/** prüfung zahlen einfaches if
+int eingabe = 11;
+
+if(eingabe < 12 && istWahr == true) {
+  System.out.println(eingabe + " ist kleiner als 12");
+}
+else{
+  System.out.println("Eingabe größer gleich 12");
+}
+*/  
+
+/** If & Else if 
+int eingabe = 9;
+boolean istWahr = false;
+
+if(istWahr == !true){
+  System.out.println("Fuck off");
+}
+else if(eingabe <= 10) {
+  System.out.println(eingabe + " ist kleiner als 10");
+}
+else {
+  System.out.println(eingabe + " ist größer als 10");
+}
+*/
+
+
+}
+
+
+
+}
