c18ac451326212f1ed4ecf66020572db3783ae92
helgehelge123 <helge-n@gmx.net>
2022-12-04 19:39:03 +0100

    created lektion 5

---
+++ Lektion5.java
@@ -0,0 +1,32 @@
+public class Lektion5 {
+    
+    private String ktoNr;
+    private double stand;
+
+        public void einzahlen (double einzahlungsbetrag){
+            this.stand += einzahlungsbetrag;
+        }
+
+        public double auszahlen (double auszahlungsbetrag){
+            this.stand -= auszahlungsbetrag;
+            return this.stand;
+        }
+
+        public void setKtoNr(String ktoNr){
+            this.ktoNr = ktoNr;
+        }
+
+        public String getKtoNr(){
+            return this.ktoNr
+        }
+
+        public void setStand(double stand){
+            this.stand = stand;
+        }
+
+        public double getStand(){
+            return this.stand
+        }
+        
+}
+
