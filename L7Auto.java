c7ada3c4c44cec9a590191aa8a5d2181416bdc86
helgehelge123 <helge-n@gmx.net>
2022-12-21 13:18:42 +0100

    added constructor

--- L7Auto.java
+++ L7Auto.java
@@ -2,15 +2,38 @@ public class L7Auto {
     
     private String fahrgestellNr;
     private int ps;
-    private String Typ;
+    private String typ;
     private float hubraum;
+
+
+//Constructor
+    public L7Auto(){
+
+    }
+// Überladen des Standartkostruktors
+    public L7Auto(String fahrgestellNr, int ps, String typ, float hubraum) {
+        this.fahrgestellNr = fahrgestellNr;
+        this.ps = ps;
+        this.typ = typ;
+        this.hubraum = hubraum;
+    }
+// Überladen funktioniert nur mit ungleichen parameterlisten
+// Würde nicht funktionieren, weil es 2 Konstruktoren mit Strings gibt
+ 
+    // public L7Auto(String fahrgestellNr){
+    // this.fahrgestellNr = fahrgestellNr;
+    // }
+    public L7Auto(String typ){
+        this.typ=typ;
+    }
+
 
 //setter
     public void setPs(int ps) {
         this.ps = ps;
     }
     public void setTyp(String typ) {
-        Typ = typ;
+        this.typ = typ;
     }
     public void setHubraum(float hubraum) {
         this.hubraum = hubraum;
@@ -19,9 +42,9 @@ public class L7Auto {
         this.fahrgestellNr = fgnr;
     }
 
-    //gibt objekt als string aus
+//gibt objekt als string aus
     public String toString(){
-        return "Fahrgestellnummer:" + this.fahrgestellNr + ", " + this.Typ + " mit " + this.hubraum + "l Hubraum und " + this.ps + "PS.";
+        return "Fahrgestellnummer:" + this.fahrgestellNr + ", " + this.typ + " mit " + this.hubraum + "l Hubraum und " + this.ps + "PS.";
     }
 
 
--- L7Verwaltung.java
+++ L7Verwaltung.java
@@ -4,14 +4,21 @@ public static void main(String[] args){
 // erstellt neues objekt
     L7Auto auto = new L7Auto();
 
-// settermethoden in action
+//  settermethoden in action
     auto.setFahrgestellNr("4711");
     auto.setPs(120);
     auto.setHubraum(2.1f);
     auto.setTyp("golf 7");
 
-// Ausgabe 
+
+// constructor test
+    L7Auto auto2 = new L7Auto("penis", 169, "golf 8", 1.23f);
+    L7Auto auto3 = new L7Auto("Golf 9");
+
+    // Ausgabe 
     System.out.println(auto.toString());
+    System.out.print(auto2.toString());
+    System.out.print(auto3.toString());
 }
 
 }
