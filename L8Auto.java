d8dff52567d85e12d99881c17236b06761909cca
helgehelge123 <helge-n@gmx.net>
2023-01-06 16:01:04 +0100

    stuff l8

--- L8Auto.java
+++ L8Auto.java
@@ -4,22 +4,14 @@ public class L8Auto extends L8Fahrzeug {
     private int ps;
     private String typ;
     private float hubraum;
+    private int gewicht;
 
 
 //Constructor
     public L8Auto(){
     }
-// // Überladen des Standartkostruktors
-//     public L8Auto(String fahrgestellNr, int ps, String typ, float hubraum) {
-//         this.fahrgestellNr = fahrgestellNr;
-//         this.ps = ps;
-//         this.typ = typ;
-//         this.hubraum = hubraum;
-//     }
 
-// Überladen des Standartkostruktors und extends thingi? 
-//Erklärung Wenn der Konstruktor aufgerufen wird, wird durch this(fahrgestellNr) der public L7Auto(String fahrgestellNr){...} konstruktor aufgerufen
-// und durch dessen this(); wird der standardkonstruktor aufgerufen. Danach geht es weiter mit this.ps.....
+
     public L8Auto(String fahrgestellNr, int ps, String typ, float hubraum) {
         this(fahrgestellNr);
         this.ps = ps;
@@ -35,6 +27,9 @@ public class L8Auto extends L8Fahrzeug {
         this.hubraum = hubraum;
     }
 
+    public L8Auto(int ps, int gewicht){
+
+    }
 // Überladen funktioniert nur mit ungleichen parameterlisten
 // Würde nicht funktionieren, weil es 2 Konstruktoren mit Strings gibt
  
@@ -60,11 +55,37 @@ public class L8Auto extends L8Fahrzeug {
     public void setFahrgestellNr(String fgnr) {
         this.fahrgestellNr = fgnr;
     }
+    public void setGewicht(int gewicht){
+        this.gewicht= gewicht;
+    }
 
 //gibt objekt als string aus
     public String toString(){
         return "Fahrgestellnummer:" + this.fahrgestellNr + ", " + this.typ + " mit " + this.hubraum + "l Hubraum und " + this.ps + "PS" + " von " + this.hersteller;
     }
 
+//Leistungsgewicht berechnen, Exeption Beispiel
+
+//Exception wird direkt im Programm gecatcht.
+/*public float berechneLeistungsgewicht(int gewicht){
+    float lg = 0;
+    try{ //kritische anweisung
+        lg = this.ps / gewicht;
+    }
+    catch(Exception e) {
+        System.out.println("division durch 0");
+    } //alles nach dem catch block, wird nicht ausgeführt, wenn fehler gefangen wird.
+    finally {
+        return lg;
+    }
+}
+*/
+//Exception wird per throw gehandled
+public float berechneLeistungsgewicht(int gewicht) throws ArithmeticException{
+    float lg = 0;
+        lg = this.ps / gewicht;
+        return lg;
+
+}
 
 }
--- L8Verwaltung.java
+++ L8Verwaltung.java
@@ -12,15 +12,29 @@ public static void main(String[] args){
 
 
 // constructor test
-    L8Auto auto2 = new L8Auto("penis", 169, "golf 8", 1.23f);
+/*    L8Auto auto2 = new L8Auto("penis", 169, "golf 8", 1.23f);
     L8Auto auto3 = new L8Auto("Golf 9");
     L8Auto auto4 = new L8Auto("vw", "penis", 169, "golf 8", 1.23f);
 
     // Ausgabe 
-    System.out.println(auto.toString());
+     System.out.println(auto.toString());
     System.out.println(auto2.toString());
     System.out.println(auto3.toString());
-    System.out.println(auto4.toString());
+    System.out.println(auto4.toString()); */
+
+    //Test ausgabe Constructor gewicht
+try{
+L8Auto auto5 = new L8Auto(105, 1400);
+    System.out.println(auto5.berechneLeistungsgewicht(2));
 }
 
+catch (ArithmeticException aex){
+    System.out.println("Fehler:" + aex );
 }
+
+}}
+
+
+
+
+
