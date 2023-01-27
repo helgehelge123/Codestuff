7c78bf8626a232ddf9e322ec340321637f8dbaee
helgehelge123 <helge-n@gmx.net>
2023-01-27 18:04:45 +0100

    Lection 9 added

---
+++ L9Person.java
@@ -0,0 +1,9 @@
+public class L9Person {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ iKunde.java
@@ -0,0 +1,5 @@
+public interface iKunde {
+    
+    public double berechneSteuern();
+
+}
